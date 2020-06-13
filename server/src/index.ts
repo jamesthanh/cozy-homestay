4435134079ab22f453cfe97b48113fbe8ec7a432
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-13 13:31:49 +0700

    added mocking interface listing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 9000;
 
-const one: number = 1;
+const one = 1;
 
 app.get('/', (_req, res) => res.send('Hello World'));
 app.listen(port);
