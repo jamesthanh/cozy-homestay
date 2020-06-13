7e5a9a4e3d0ec4bd9fea9984752a330de49755dc
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-13 14:33:21 +0700

    added apollo server and graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import { listings } from './listings';
 const app = express();
 const port = 9000;
 
-const one = 1;
+const server = new ApolloServer({ typeDefs: ``, resolvers: {} });
+server.applyMiddleware({ app, path: '/api' });
+app.use(bodyParser.json());
 
 app.get('/', (_req, res) => res.send('Hello World'));
+
+app.get('/listings', (_req, res) => {
+  return res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send('failed to delete listing');
+});
+
 app.listen(port);
 console.log(`[app]: http://localhost:${port}`);
