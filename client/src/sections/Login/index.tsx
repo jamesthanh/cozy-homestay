dfc5a46445af131fcae503de7603f95972f3fa9b
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-21 16:59:07 +0700

    finished log in with google and redirect the user

--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,12 +1,88 @@
-import React from 'react';
-import { Card, Layout, Typography, Button } from 'antd';
+import React, { useEffect, useRef } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import { Card, Layout, Typography, Spin } from 'antd';
+import { Viewer } from '../../lib/types';
+import { ErrorBanner } from '../../lib/components';
+import {
+  dispatchSuccessNotification,
+  displayErrorMessage,
+} from '../../lib/utils';
+import { AUTH_URL } from '../../lib/graphql/queries';
+import { LOG_IN } from '../../lib/graphql/mutations';
+import { AuthUrl as AuthUrlData } from '../../lib/graphql/queries/authUrl/__generated__/AuthUrl';
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from '../../lib/graphql/mutations/LogIn/__generated__/LogIn';
 import googleLogo from './assets/google_logo.jpg';
+
+interface Props {
+  setViewer: (viewer: Viewer) => void;
+}
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
-export const Login = () => {
+export const Login = ({ setViewer }: Props) => {
+  const client = useApolloClient();
+  const [
+    logIn,
+    { data: logInData, loading: logInLoading, error: logInError },
+  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+        dispatchSuccessNotification('Logged in successfully!');
+      }
+    },
+  });
+
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: {
+            code,
+          },
+        },
+      });
+    }
+  }, []);
+
+  const handleAuthorizate = async () => {
+    try {
+      const { data } = await client.query<AuthUrlData>({
+        query: AUTH_URL,
+      });
+      window.location.href = data.authUrl;
+    } catch (error) {
+      displayErrorMessage('Sorry, encountered errors, please retry later');
+    }
+  };
+
+  if (logInLoading) {
+    return (
+      <Content className='log-in'>
+        <Spin size='large' tip='Logging you in' />
+      </Content>
+    );
+  }
+
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+    return <Redirect to={`/user/${viewerId}`} />;
+  }
+
+  const LogInErrorBannerElement = logInError ? (
+    <ErrorBanner description='Sorry, encountered errors, please retry later' />
+  ) : null;
+
   return (
     <Content className='log-in'>
+      {LogInErrorBannerElement}
       <Card className='log-in-card'>
         <div className='log-in-card__intro'>
           <Title level={3} className='log-in-card__intro-title'>
@@ -21,7 +97,10 @@ export const Login = () => {
             Sign in with your google account to start using our services
           </Text>
         </div>
-        <Button className='log-in-card__google-button'>
+        <button
+          className='log-in-card__google-button'
+          onClick={handleAuthorizate}
+        >
           <img
             src={googleLogo}
             alt='Google Logon'
@@ -30,7 +109,7 @@ export const Login = () => {
           <span className='log-in-card__google-button-text'>
             Sign in with Google
           </span>
-        </Button>
+        </button>
         <Text type='secondary'>
           By signing, you will be redirected to your Google content form.
         </Text>
