858ccf49edfe53391f5eb09c73b2669e0eccc533
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-23 09:35:31 +0700

    added user grapqh fields

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,4 +1,4 @@
 import merge from 'lodash.merge';
-import { viewerResolvers } from './Viewer/index';
-
-export const resolvers = merge(viewerResolvers);
+import { viewerResolvers } from './Viewer';
+import { userResolvers } from './User';
+export const resolvers = merge(viewerResolvers, userResolvers);
